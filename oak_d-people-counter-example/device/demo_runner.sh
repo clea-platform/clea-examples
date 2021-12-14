20a82e88b789e747ece149c6f5e59c4ae5abbad7
Luca Di Mauro <luca.dimauro@seco.com>
2021-12-14 12:03:19 +0100

    Updated demo_runner.sh script with creation of virtualenv and persistence directory if not existing

--- oak_d-people-counter-example/device/demo_runner.sh
+++ oak_d-people-counter-example/device/demo_runner.sh
@@ -3,5 +3,18 @@
 
 cd $(dirname $0)
 
+# Creating virtualenv if not existing
+if [[ ! -d "./venv" ]] ; then
+    virtualenv venv
+    source venv/bin/activate
+    pip3 install -r requirements.txt
+    deactivate
+fi
+
+# Creating 'astarte_persistence.d' if not existing
+if [[ ! -d "./astarte_persistence.d" ]] ; then
+    mkdir astarte_persistence.d
+fi
+
 source venv/bin/activate
 python3 main.py
