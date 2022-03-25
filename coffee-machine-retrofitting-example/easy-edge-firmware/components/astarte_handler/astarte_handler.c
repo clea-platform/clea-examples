85a5acca0fa444ab409bd1e6e11d999badf9e98e
Luca Di Mauro <dima.91@live.it>
2022-03-25 10:58:18 +0100

    Added 'easy-edge-firmware' project skeleton

---
+++ coffee-machine-retrofitting-example/easy-edge-firmware/components/astarte_handler/astarte_handler.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "astarte_handler.h"
+
+void func(void)
+{
+
+}
---
+++ coffee-machine-retrofitting-example/easy-edge-firmware/components/emulated_coffe_machine_sensor/emulated_coffe_machine_sensor.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "emulated_coffe_machine_sensor.h"
+
+void func(void)
+{
+
+}
